d1ddef44eb405498ed2d2041991e38c5eb5fa919
anirban2404 <anirban_bhattacharjee@hotmail.com>
2018-04-10 06:09:41 +0000

    word2vec_google added

---
+++ word2vec/word2vec_Google.py
@@ -0,0 +1,61 @@
+# import the necessary packages
+from gensim.models import KeyedVectors
+import flask
+import io
+
+# initialize our Flask application and the Keras model
+app = flask.Flask(__name__)
+model = None
+
+def load_model():
+    # load the pre-trained word2vec model (here we are using a model
+    # pre-trained)
+    global model
+    filename = 'GoogleNews-vectors-negative300.bin'
+    model = KeyedVectors.load_word2vec_format(filename, binary=True)
+    
+@app.route("/predict_most_similar", methods=["POST"])
+def predict_most_similar():
+    # initialize the data dictionary that will be returned from the
+    # view
+    data = {"success": False}
+    # ensure an image was properly uploaded to our endpoint
+    if flask.request.method == "POST":
+        text_label1 = flask.request.form['label1']
+        processed_label1 = text_label1.lower()
+        print(processed_label1)
+        text_label2 = flask.request.form['label2']
+        processed_label2 = text_label2.lower()     
+        text_label3 = flask.request.form['label3']
+        processed_label3 = text_label3.lower()
+        data["predictions"] = []
+        preds = model.most_similar_cosmul(positive=[processed_label1, processed_label2], negative=[processed_label3], topn=1)
+        # indicate that the request was a success
+        data["success"] = True
+        data["predictions"].append(preds)
+        # return the data dictionary as a JSON response
+    return flask.jsonify(data)
+    
+@app.route("/predict_doesnt_match", methods=["POST"])
+def predict_doesnt_match():
+    # initialize the data dictionary that will be returned from the
+    # view
+    result = {"success": False}
+    # ensure an image was properly uploaded to our endpoint
+    if flask.request.method == "POST":
+        text_label4 = flask.request.form['label4']
+        processed_label4 = text_label4.lower() #"breakfast cereal dinner lunch"
+        result["predictions"] = []
+        pred = model.doesnt_match(processed_label4.split())
+        # indicate that the request was a success
+        result["success"] = True
+        result["predictions"].append(pred)
+    return flask.jsonify(result)
+
+# if this is the main thread of execution first load the model and
+# then start the server
+if __name__ == "__main__":
+    print(("* Loading Keras model and Flask starting server..."
+        "please wait until server has fully started"))
+    load_model()
+    app.run(host='0.0.0.0', port=7010)
